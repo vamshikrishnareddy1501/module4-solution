b6b0653d24fee5a14ff153215d10137b09611e16
vamshikrishnareddy1501 <63491078+vamshikrishnareddy1501@users.noreply.github.com>
2020-05-01 17:02:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ Good Bye Jimmy Anderson
 */
 
 (function() {
-    var names = ["Praharsha", "Johnson", "Jerome Taylor", "Jason Roy", "Virat Kohli", "Mahesh Babu", "Dale Steyn", "SRH", "Brain Laura", "Jimmy Anderson"];
+    var names = ["vamshi krishna", "Rohith", "K L Rahul", "Dhoni", "Virat Kohli", "Prabhas", "Dale Steyn", "SRH", "Brain Laura", "Jimmy Anderson"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {

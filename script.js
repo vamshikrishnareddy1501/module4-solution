343bba8342eab971e64ed2b3fe0a509dedc98fa3
vamshikrishnareddy1501 <63491078+vamshikrishnareddy1501@users.noreply.github.com>
2020-05-01 17:06:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-/*
-Solution of assignment 4:
-Expected output:
-Hello Praharsha
-Good Bye Johnson
-Good Bye Jerome Taylor
-Good Bye Jason Roy
-Hello Virat Kohli
-Hello Mahesh Babu
-Hello Dale Steyn
-Hello SRH
-Hello Brain Laura
-Good Bye Jimmy Anderson
-*/
+
 
 (function() {
-    var names = ["vamshi krishna", "Rohith", "K L Rahul", "Dhoni", "Virat Kohli", "Prabhas", "Dale Steyn", "SRH", "Brain Laura", "Jimmy Anderson"];
+    var names = ["vamshi krishna", "rohith", "Taylor", "Bhuvneshwar", "K L Rahul", "Dhoni", "Dale Steyn", "SRH", "Brain Laura", "Jimmy Anderson"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
